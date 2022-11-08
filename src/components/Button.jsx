b4bae21c0676daf024ae6216caa6eeac459dc073
Sonia Akter <soniaahmed01795@gmail.com>
2022-11-08 18:43:11 +0600

    Add files via upload

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`bg-blue-gradient py-4 px-6 font-poppins font-medium text-[18px] 
+      text-primary outline-none ${styles} rounded-[10px]`}
+    >
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
